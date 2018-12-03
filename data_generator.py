111fadeea6e7a7574dc6712f30f9bef21d47f1d8
song_hz <hs50@rice.edu>
2018-12-03 15:23:58 -0600

    start

---
+++ data_generator.py
@@ -0,0 +1,172 @@
+import numpy as np
+import random as random
+
+
+class Data:
+	def __init__(self, nid, hot_threshold=10):
+		self.__network_id = nid
+		self.__directory = "./data/"
+		self.__edge_file = nid+".edges"
+		self.__feature_field_file = nid+".featnames"
+		self.__feature_file = nid+".feat"
+		self.__hot_threshold = hot_threshold
+
+		self.__raw_friends_mat = {}
+		self.__feature_field = {}
+		self.__friends_mat = {}
+		self.__user_id_list = []
+		self.__id_feature = {}
+		self.__feature = {}
+		self.__test_mat = []
+
+		# read in features' dimension
+		with open(self.__directory+self.__feature_field_file) as fin:
+			for line in fin:
+				feature_tuple = line.strip().split(" ")
+				field_idx = feature_tuple[0]
+				feature_name = feature_tuple[1]
+				if feature_name in self.__feature_field:
+					self.__feature_field[feature_name][1] = int(field_idx)
+				else:
+					self.__feature_field[feature_name] = [int(field_idx), int(field_idx)]
+
+		# build the friend matrix, row: uid, col: friend_id
+		with open(self.__directory+self.__edge_file) as fin:
+			for line in fin:
+				id_tuple = line.strip().split(" ")
+				if id_tuple[0] in self.__friends_mat:
+					self.__friends_mat[id_tuple[0]].append(id_tuple[1])
+					self.__raw_friends_mat[id_tuple[0]].append(id_tuple[1])
+				else:
+					self.__friends_mat[id_tuple[0]] = [id_tuple[1]]
+					self.__raw_friends_mat[id_tuple[0]] = [id_tuple[1]]
+
+		# read in users' features
+		with open(self.__directory+self.__feature_file) as fin:
+			for line in fin:
+				feature_tuple = line.strip().split(" ")
+				self.__feature[feature_tuple[0]] = []
+				self.__user_id_list.append(feature_tuple[0])
+				for one_hot in feature_tuple[1:]:
+					self.__feature[feature_tuple[0]].append(int(one_hot))
+
+		# generate the id feature
+		for idx, user_id in enumerate(self.__user_id_list):
+			one_hot = [0]*len(self.__user_id_list)
+			one_hot[idx] = 1
+			self.__id_feature[user_id] = one_hot
+
+		print("Generating Test Set by Leave One Out")
+		# leave one out as the test set
+		# avoid isolated nodes, so some users will be skipped
+		for user_id in self.__friends_mat:
+			if len(self.__friends_mat[user_id]) >= self.__hot_threshold:
+				# sample a friend id of this user
+				rand_fid = self.__sample_friend_id(user_id)
+				# move this link from the original friend matrix to the test friend matrix
+				self.__test_mat.append([user_id, rand_fid])
+				self.__friends_mat[user_id].remove(rand_fid)
+				self.__friends_mat[rand_fid].remove(user_id)
+
+	def __sample_friend_id(self, user_id):
+		num_friend = len(self.__friends_mat[user_id])
+		random_id = self.__friends_mat[user_id][random.randint(0, num_friend-1)]
+		# remove this link only when this removal won't cause an isolate node
+		while len(self.__friends_mat[random_id]) <= 1:
+			random_id = self.__friends_mat[user_id][random.randint(0, num_friend - 1)]
+		return random_id
+
+	def __sample_stranger_id(self, user_id):
+		random_id = self.__user_id_list[random.randint(0, len(self.__user_id_list)-1)]
+		# if random_id is a friend of user, sample again
+		while random_id in self.__friends_mat[user_id] or random_id == user_id:
+			random_id = self.__user_id_list[random.randint(0, len(self.__user_id_list) - 1)]
+		return random_id
+
+	def __fill_X(self, X, data):
+		X.append(data["id"])
+		for feature_name in self.__feature_field:
+			X.append(data[feature_name])
+
+	def sample(self):
+		user_data = {}
+		friend_data = {}
+		stranger_data = {}
+
+		for feature_name in self.__feature_field:
+			user_data[feature_name] = []
+			friend_data[feature_name] = []
+			stranger_data[feature_name] = []
+		user_data["id"] = []
+		friend_data["id"] = []
+		stranger_data["id"] = []
+
+		train_num = 0
+		for user_id in self.__friends_mat:
+			for friend_id in self.__friends_mat[user_id]:
+				# sample a stranger_id
+				stranger_id = self.__sample_stranger_id(user_id)
+
+				for feature_name in self.__feature_field:
+					start_idx = self.__feature_field[feature_name][0]
+					end_idx = self.__feature_field[feature_name][1]
+					user_data[feature_name].append(self.__feature[user_id][start_idx:end_idx+1])
+					friend_data[feature_name].append(self.__feature[friend_id][start_idx:end_idx+1])
+					stranger_data[feature_name].append(self.__feature[stranger_id][start_idx:end_idx+1])
+
+				user_data["id"].append(self.__id_feature[user_id])
+				friend_data["id"].append(self.__id_feature[friend_id])
+				stranger_data["id"].append(self.__id_feature[stranger_id])
+				train_num += 1
+
+		X = []
+		self.__fill_X(X, user_data)
+		self.__fill_X(X, friend_data)
+		self.__fill_X(X, stranger_data)
+		return [X, train_num]
+
+	def get_input_dimension(self):
+		input_dimension = {}
+		for feature_name in self.__feature_field:
+			start_idx = self.__feature_field[feature_name][0]
+			end_idx = self.__feature_field[feature_name][1]
+			input_dimension[feature_name] = end_idx - start_idx + 1
+		input_dimension["id"] = len(self.__user_id_list)
+		return input_dimension
+
+	def get_testset(self):
+		return self.__test_mat
+
+	def get_test_feature(self, user_id, friend_id):
+
+		# one positive case
+
+		X_u = [self.__id_feature[user_id]]
+		X_f = [self.__id_feature[friend_id]]
+		for feature_name in self.__feature_field:
+			start_idx = self.__feature_field[feature_name][0]
+			end_idx = self.__feature_field[feature_name][1]
+			X_u.append(self.__feature[user_id][start_idx:end_idx + 1])
+			X_f.append(self.__feature[friend_id][start_idx:end_idx + 1])
+
+		positive = [X_u+X_f]
+
+		# all negative cases
+		negative = []
+		for stranger_id in self.__user_id_list:
+			if stranger_id != friend_id and stranger_id != user_id and (stranger_id not in self.__raw_friends_mat[user_id]):
+				X_s = [self.__id_feature[stranger_id]]
+				for feature_name in self.__feature_field:
+					start_idx = self.__feature_field[feature_name][0]
+					end_idx = self.__feature_field[feature_name][1]
+					X_s.append(self.__feature[stranger_id][start_idx:end_idx + 1])
+				negative.append(X_u+X_s)
+
+		res = []
+		for idx in positive[0]:
+			res.append([idx])
+		for row in negative:
+			for idx, field in enumerate(row):
+				res[idx].append(field)
+
+		return res
---
+++ model.py
@@ -0,0 +1,144 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow import keras
+import data_generator
+import keras.backend as K
+from sklearn.metrics import roc_auc_score
+
+layers = keras.layers
+embedding_dim = 10
+epoch_num = 1000
+
+
+def wide_part(input_a, input_b, embedding_map_list, first_order_weight):
+	embedding_a_list = []
+	embedding_b_list = []
+	input_list = []
+	for idx, i in enumerate(input_a):
+		input_list.append(i)
+		embedding_a_list.append(embedding_map_list[idx](i))
+	for idx, i in enumerate(input_b):
+		input_list.append(i)
+		embedding_b_list.append(embedding_map_list[idx](i))
+
+	merged_inputs_first_order = layers.concatenate(input_list, axis=-1)
+	first_order_output = first_order_weight(merged_inputs_first_order)
+
+	# Generate the inner product layer
+	inner_product_list = [layers.dot([embedding_a_list[0], embedding_b_list[0]], axes=1)]
+	for i in range(1, len(embedding_a_list)):
+		for j in range(i+1, len(embedding_a_list)):
+			inner_product_list.append(layers.dot([embedding_a_list[i], embedding_a_list[j]], axes=1))
+	for i in range(1, len(embedding_b_list)):
+		for j in range(i+1, len(embedding_b_list)):
+			inner_product_list.append(layers.dot([embedding_b_list[i], embedding_b_list[j]], axes=1))
+
+	for i in range(1, len(embedding_a_list)):
+		for j in range(1, len(embedding_b_list)):
+			inner_product_list.append(layers.dot([embedding_a_list[i], embedding_b_list[j]], axes=1))
+
+	inner_product_list.append(first_order_output)
+	fm_output = layers.add(inner_product_list)
+	return fm_output
+	# return layers.dot([embedding_map_list[0](input_a[0]), embedding_map_list[0](input_b[0])], axes=1)
+
+
+def deep_part(input_a, input_b, embedding_map_list, deep_layers):
+	embedding_list = []
+	for idx, i in enumerate(input_a):
+		embedding_list.append(embedding_map_list[idx](i))
+	for idx, i in enumerate(input_b):
+		embedding_list.append(embedding_map_list[idx](i))
+
+	output = layers.concatenate(embedding_list)
+
+	for layer in deep_layers:
+		output = layer(output)
+	return output
+
+
+def get_auc(model, test_set, data):
+	auc = 0.0
+	for idx, single_sample in enumerate(test_set):
+		LOO_set = data.get_test_feature(single_sample[0], single_sample[1])
+		score_list = model.predict(LOO_set)
+		target = [0 for _ in score_list]
+		target[0] = 1
+		# unfold score_list
+		score_list = [number for _number_ in score_list for number in _number_]
+		if len(score_list) > 1:
+			auc += roc_auc_score(target, score_list)
+	auc /= len(test_set)
+	return auc
+
+
+def main():
+	debug_switch = False
+
+	print("Reading Data ...")
+	network_id = "348"
+	data = data_generator.Data(network_id, 2)
+
+	# get the test set and the size of it
+	test_set = data.get_testset()
+
+	print("Building Model ...")
+	input_dimension = data.get_input_dimension()
+
+	user_inputs = []
+	friend_inputs = []
+	stranger_inputs = []
+
+	# the input layer and shared embedding layers
+	id_dim = input_dimension["id"]
+	user_inputs.append(layers.Input(shape=(id_dim,)))
+	friend_inputs.append(layers.Input(shape=(id_dim,)))
+	stranger_inputs.append(layers.Input(shape=(id_dim,)))
+	embedding_map_list = [layers.Dense(embedding_dim)]
+
+	for feature_name in input_dimension:
+		if feature_name != "id":
+			dim = input_dimension[feature_name]
+			user_inputs.append(layers.Input(shape=(dim, )))
+			friend_inputs.append(layers.Input(shape=(dim, )))
+			stranger_inputs.append(layers.Input(shape=(dim, )))
+			embedding_map_list.append(layers.Dense(embedding_dim))
+
+	first_order_weight = layers.Dense(1, name="first_order")
+
+	# the deep layer stack
+	layer_size = [32, 32, 32]
+	layer_stack = []
+
+	for tensor_size in layer_size:
+		layer_stack.append(layers.Dense(tensor_size, activation="relu"))
+	layer_stack.append(layers.Dense(1))
+
+	# friend part
+	friend_wide_score = wide_part(user_inputs, friend_inputs, embedding_map_list, first_order_weight)
+	friend_deep_score = deep_part(user_inputs, friend_inputs, embedding_map_list, layer_stack)
+	friend_score = layers.add([friend_wide_score, friend_deep_score])
+
+	# stranger part
+	stranger_wide_score = wide_part(user_inputs, stranger_inputs, embedding_map_list, first_order_weight)
+	stranger_deep_score = deep_part(user_inputs, stranger_inputs, embedding_map_list, layer_stack)
+	stranger_score = layers.add([stranger_wide_score, stranger_deep_score])
+
+	# friend_score - stranger_score
+	deepfm_out = layers.subtract([friend_score, stranger_score], name="diff_layer")
+	deepfm_out = layers.Dense(1, activation="sigmoid", trainable=False, kernel_initializer="ones", name="sigmoid_output")(deepfm_out)
+
+	score_model = keras.Model(inputs=user_inputs+friend_inputs, outputs=friend_wide_score)
+	deepfm_model = keras.Model(inputs=user_inputs+friend_inputs+stranger_inputs, outputs=deepfm_out)
+	deepfm_model.compile(loss='binary_crossentropy', optimizer='adam')
+
+	print("AUC before training:", get_auc(score_model, test_set, data))
+	for epoch_idx in range(epoch_num):
+		X, train_num = data.sample()
+		deepfm_model.fit(X, np.ones(train_num), batch_size=32, epochs=1, shuffle=True, verbose=0)
+		print("AUC:", get_auc(score_model, test_set, data))
+
+
+if __name__ == '__main__':
+	main()
+
